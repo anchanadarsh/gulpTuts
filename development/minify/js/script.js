6d7345ea05e89f472b84ce96ac4f74d5117f9016
adarsh anchan <anchanadarsh@gmail.com>
2018-02-19 18:22:08 +0530

    minify js css

---
+++ development/minify/js/script.js
@@ -0,0 +1,49 @@
+// All custom codes with comment
+
+$(document).ready(function () {
+    //banner effect
+    $("#banner .banner-inner h1 span").addClass("animated");
+    $("#banner .banner-inner h1 span").mouseover(function () {
+        $("#banner .banner-inner h1 span").removeClass("shake");
+        $(this).addClass("shake");
+    });
+
+    //modal bg blur effect
+    $("#mywork").on("click", ".zoom-modal", function () {
+        $(".makeblur").css("filter", "blur(25px)");
+    });
+    $(".project-modal-outer").on("click", ".mod-close", function () {
+        $(".makeblur").css("filter", "blur(0px)");
+    });
+
+
+    //switch button effect
+    $(".switch-on-off-button").click(function () {
+        $(".light-off-overlay").toggleClass("oz-0");
+        $(this).toggleClass("switch-color-red");
+        $(".switch-on-shadow").toggleClass("switch-color-red");
+        $(".switch-on-off-button p").html("Switch On");
+        $(".switch-color-red p").html("Switch Off");
+        $(this).css("top", "35px");
+        $(".switch-on-shadow").css("top", "2px");
+        setTimeout(function () {
+            $(".switch-on-off-button").css("top", "30px");
+            $(".switch-on-shadow").css("top", "7px");
+        }, 100);
+    });
+
+
+    //skill-hover-effect
+    $("#myskills").on("mouseover", ".skill-box-out", function () {
+        var getcolor = $(this).find("img").attr("setcolor");
+        //        $(".skill-box img").addClass("bnw");
+        //        $(this).find("img").addClass("invert-transform");
+        $(".skill-overlay-bg").css("background", getcolor);
+    });
+    $("#myskills").on("mouseout", ".skill-box-out", function () {
+        $(".skill-box img").removeClass("bnw");
+        $(this).find("img").removeClass("invert-transform");
+        $(".skill-overlay-bg").css("background", "transparent");
+    });
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minifyjs = require('gulp-uglify');
+var minifycss = require('gulp-minify-css');
+
 gulp.task('sass', function () {
-    return gulp.src('development/style.scss')
+    return gulp.src('development/scss-css/style.scss')
         .pipe(sass())
-        .pipe(gulp.dest('development'));
+        .pipe(gulp.dest('development/scss-css'));
 });
+
+gulp.task('script', function () {
+    return gulp.src('development/minify/js/*.js').pipe(minifyjs()).pipe(gulp.dest('development/minify/minified/js'));
+});
+
+gulp.task('styles', function () {
+    return gulp.src('development/minify/css/style.css').pipe(minifycss()).pipe(gulp.dest('development/minify/minified/css'));
+});
+
+gulp.task('minifyall', ['styles', 'script']);
